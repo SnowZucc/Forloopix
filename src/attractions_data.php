730d091377e0e1a670b7f3ec51b4a2ed186d98b5
Zucc <louis.alexandre@eleve.isep.fr>
2025-06-13 12:14:12 +0200

    Images locales et compressées

--- src/attractions_data.php
+++ src/attractions_data.php
@@ -4,7 +4,7 @@
 $attractions = [
     'goudurix' => [
         'name' => 'Goudurix',
-        'image' => 'https://static.wixstatic.com/media/84686c_96b15b58df61431aa41c71cfbb4789cf~mv2.jpg/v1/fit/w_1000%2Ch_768%2Cal_c%2Cq_80/file.jpg',
+        'image' => '../../public/assets/img/attractions/Goudurix.jpg',
         'seats' => 8,
         'led_positions' => [
             'sensor-1' => ['top' => '31%', 'left' => '20%'],
@@ -14,7 +14,7 @@ $attractions = [
     ],
     'oziris' => [
         'name' => 'OzIris',
-        'image' => 'https://www.backtothegeek.com/wp-content/uploads/2015/08/OZIRIS-c-H.-Cussot.jpg',
+        'image' => '../../public/assets/img/attractions/OzIris.jpg',
         'seats' => 12,
         'led_positions' => [
             'sensor-1' => ['top' => '20%', 'left' => '25%'],
@@ -24,7 +24,7 @@ $attractions = [
     ],
     'tonnerre-2-zeus' => [
         'name' => 'Tonnerre 2 Zeus',
-        'image' => 'https://lelutecien.shop/img/cms/T2Z/tonnerre%202%20zeus%20parc%20asterix%202022%203.jpg',
+        'image' => '../../public/assets/img/attractions/Tonnerre2Zeus.jpg',
         'seats' => 10,
         'led_positions' => [
             'sensor-1' => ['top' => '50%', 'left' => '15%'],
@@ -34,7 +34,7 @@ $attractions = [
     ],
     'pegase-express' => [
         'name' => 'Pégase Express',
-        'image' => 'https://www.programme-tv.net/imgre/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2FTEL.2Enews.2F2018.2F01.2F11.2F37455c02-85c3-455c-a07c-78d07dc95d44.2Ejpeg/1200x600/crop-from/top/quality/80/parc-asterix-decouvrez-pegase-express-la-toute-nouvelle-attraction-photos.jpg',
+        'image' => '../../public/assets/img/attractions/Pegase-Express.jpg',
         'seats' => 8,
         'led_positions' => [
             'sensor-1' => ['top' => '60%', 'left' => '10%'],
